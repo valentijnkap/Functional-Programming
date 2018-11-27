4d7a0b36e6e587b07d9c23311d380775e9e01d59
Valentijn Kap <info@valentijnkap.nl>
2018-11-27 13:52:04 +0100

    added interactivity

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,4 @@
-import * as d3 from 'd3'
+import * as d3 from "d3"
 
 // Set the size of the chart
 const width = 1000
@@ -6,12 +6,12 @@ const height = 600
 const padding = 80
 
 // Time parsing
-const timeParse      =   d3.timeParse("%Y %m")
-const timeFormat     =   d3.timeFormat("%B")
+const timeParse = d3.timeParse("%Y %m")
+const timeFormat = d3.timeFormat("%B")
 
 // Mapping the months
 const map = {
-  'januari': '01',
+  "januari": "01",
   'februari': '02',
   'maart': '03',
   'april': '04',
@@ -46,169 +46,252 @@ d3.csv("data/data.csv")
       vraagprijs: Number(data.vraagprijs)
     }
   })
-  .get(function(err, dataset) {
-    console.log(dataset)
-    if (!err) {
-      const amsterdam = dataset.filter((d) => {
-        return d.regio === "Amsterdam"
-      })
-    
-      const denhaag = dataset.filter((d) => {
-        return d.regio === "'s-Gravenhage (gemeente)"
-      })
-    
-      const rotterdam = dataset.filter((d) => {
-        return d.regio === "Rotterdam"
-      })
-    
-      const utrecht = dataset.filter((d) => {
-        return d.regio === "Utrecht (gemeente)"
-      }) 
+  .get(renderChart)
 
-      // Seting up scales
-      const xScale = d3.scaleTime()
+// Create a function to render the whole chart
+function renderChart (err, dataset) {
+  console.log(dataset)
+  if (!err) {
+    const amsterdam = dataset.filter((d) => {
+      return d.regio === "Amsterdam"
+    })
+  
+    const denhaag = dataset.filter((d) => {
+      return d.regio === "'s-Gravenhage (gemeente)"
+    })
+  
+    const rotterdam = dataset.filter((d) => {
+      return d.regio === "Rotterdam"
+    })
+  
+    const utrecht = dataset.filter((d) => {
+      return d.regio === "Utrecht (gemeente)"
+    }) 
+
+    // Seting up scales
+    const xScale = d3.scaleTime()
+    .domain([
+      d3.min(dataset, (d) => {
+        return d.perioden
+      }),
+      d3.max(dataset, (d) => {
+        return d.perioden
+      })
+    ])
+    .range([padding, width - padding])
+
+    const yScale = d3.scaleLinear()
       .domain([
-        d3.min(dataset, function(d) {
-          return d.perioden
-        }),
-        d3.max(dataset, function(d) {
-          return d.perioden
+        0, d3.max(dataset, (d) => {
+          return d.aantal
         })
       ])
-      .range([padding, width - padding])
+      .range([height - padding, padding])
+    
+    // Create Axes
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(12)
+      .tickFormat(timeFormat)
 
+    const yAxis = d3.axisLeft(yScale)
+      .ticks(12)
+    
+    // Create axis
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0," + (height - padding) + ")")
+      .call(xAxis)
+
+    svg.append("g")
+      .attr("class", "y-axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(yAxis)
+    
+    // Create Line
+    const line = d3.line()
+      .x((d) => {
+        return xScale(d.perioden)
+      })
+      .y((d) => {
+        return yScale(d.aantal)
+      })
+
+    function createLine(data, className, color) {
+      svg.append("path")
+        .datum(data)
+        .attr("class", className)
+        .attr('stroke', color)
+        .attr('stroke-width', 5)
+        .attr('fill', 'none')
+        .attr('d', line);
+    }
+
+    function createDots(data, className, color) {
+      svg.append('g').attr("class", className)
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('r', '5px')
+        .attr('cx', (d) => {
+          return xScale(d.perioden)
+        })
+        .attr('cy', (d) => {
+          return yScale(d.aantal)
+        })
+        .attr('fill', color)
+        .attr('stroke-width', 1)
+        .attr('stroke', 'white')
+    }
+
+    // Visualization for data: amsterdam  
+    createLine(amsterdam, "amsterdam-line", "#73FF36")
+    createDots(amsterdam, "amsterdam-dots", "#73FF36")
+
+    // Visualization for data: rotterdam 
+    createLine(rotterdam, "rotterdam-line", "red")
+    createDots(rotterdam, "rotterdam-dots", "red")
+
+    // Visualization for data: denhaag 
+    createLine(denhaag, "denhaag-line", "blue")
+    createDots(denhaag, "denhaag-dots", "blue")
+
+    // Visualization for data: utrecht 
+    createLine(utrecht, "utrecht-line", "yellow")
+    createDots(utrecht, "utrecht-dots", "yellow")
+    
+    d3.select('.price').on('click', changeToPrice)
+
+    function changeToPrice() {
+      // Change scales  
       const yScale = d3.scaleLinear()
         .domain([
-          0, d3.max(dataset, function(d) {
+          0, d3.max(dataset, (d) => {
+            return d.vraagprijs
+          })
+        ])
+        .range([height - padding, padding])
+      
+      const yAxis = d3.axisLeft(yScale)
+        .ticks(12)
+      
+      // Update axis
+      svg.select(".y-axis")
+        .transition()
+        .duration(900)
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis)
+      
+      const linePrice = d3.line()
+        .x((d) => {
+          return xScale(d.perioden)
+        })
+        .y((d) => {
+          return yScale(d.vraagprijs)
+        })
+      
+      function updateLine(data, selector) {
+        svg.select(selector)
+          .datum(data)
+          .transition()
+          .duration(900)
+          .attr('d', linePrice)
+      }
+
+      function updateDots(data, selector) {
+        svg.select(selector)
+          .selectAll("circle")
+          .data(data)
+          .transition()
+          .duration(900)
+          .attr("cy", (d) => {
+            return yScale(d.vraagprijs)
+          })
+      }
+
+      // Update data amsterdam
+      updateLine(amsterdam, "path.amsterdam-line")
+      updateDots(amsterdam, "g.amsterdam-dots")
+
+      // Update data rotterdam
+      updateLine(rotterdam, "path.rotterdam-line")
+      updateDots(rotterdam, "g.rotterdam-dots")
+
+      // Update data denhaag
+      updateLine(denhaag, "path.denhaag-line")
+      updateDots(denhaag, "g.denhaag-dots")
+
+      // Update data utrecht
+      updateLine(utrecht, "path.utrecht-line")
+      updateDots(utrecht, "g.utrecht-dots")
+    }
+
+    d3.select(".amount").on("click", changetoAmount)
+
+    function changetoAmount() {
+      // Change scales  
+      const yScale = d3.scaleLinear()
+        .domain([
+          0, d3.max(dataset, (d) => {
             return d.aantal
           })
         ])
         .range([height - padding, padding])
       
-      // Create Axes
-      const x_axis = d3.axisBottom(xScale)
-        .ticks(12)
-        .tickFormat(timeFormat)
-
-      const y_axis = d3.axisLeft(yScale)
+      const yAxis = d3.axisLeft(yScale)
         .ticks(12)
       
-      // Create axis
-      svg.append("g")
-        .attr("transform", "translate(0," + (height - padding) + ")")
-        .call(x_axis)
-
-      svg.append("g")
-        .attr("transform", "translate(" + padding + ",0)")
-        .call(y_axis)
-      
-      // Create Line
-      const line = d3.line()
-        .x(function(d) {
-          return xScale(d.perioden);
-        })
-        .y(function(d) {
-          return yScale(d.aantal);
-        })
-      
-      // Data for Amsterdam
-      svg.append("path")
-        .datum(amsterdam)
+      // Update axis
+      svg.select(".y-axis")
         .transition()
-        .attr('stroke', '#73FF36')
-        .attr('stroke-width', 5)
-        .attr('fill', 'none')
-        .attr('d', line);
+        .duration(900)
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis)
       
-      svg.append('g').selectAll('circle')
-        .data(amsterdam)
-        .enter()
-        .append('circle')
-        .attr('r', '5px')
-        .attr('cx', function(d) {
+      const linePrice = d3.line()
+        .x((d) => {
           return xScale(d.perioden)
         })
-        .attr('cy', function(d) {
+        .y((d) => {
           return yScale(d.aantal)
         })
-        .attr('fill', '#73FF36')
-        .attr('stroke-width', 1)
-        .attr('stroke', 'white')
+      
+      function updateLine(data, selector) {
+        svg.select(selector)
+          .datum(data)
+          .transition()
+          .duration(900)
+          .attr('d', linePrice)
+      }
 
-      // Data for Rotterdam
-      svg.append("path")
-        .datum(rotterdam)
-        .attr('stroke', 'red')
-        .attr('stroke-width', 5)
-        .attr('fill', 'none')
-        .attr('d', line);
-      
-      svg.append('g').selectAll('circle')
-        .data(rotterdam)
-        .enter()
-        .append('circle')
-        .attr('r', '5px')
-        .attr('cx', function(d) {
-          return xScale(d.perioden)
-        })
-        .attr('cy', function(d) {
-          return yScale(d.aantal)
-        })
-        .attr('fill', 'red')
-        .attr('stroke-width', 1)
-        .attr('stroke', 'white')
+      function updateDots(data, selector) {
+        svg.select(selector)
+          .selectAll("circle")
+          .data(data)
+          .transition()
+          .duration(900)
+          .attr("cy", (d) => {
+            return yScale(d.aantal)
+          })
+      }
 
-      // Data for Den Haag
-      svg.append("path")
-        .datum(denhaag)
-        .attr('stroke', 'blue')
-        .attr('stroke-width', 5)
-        .attr('fill', 'none')
-        .attr('d', line);
-      
-      svg.append('g').selectAll('circle')
-        .data(denhaag)
-        .enter()
-        .append('circle')
-        .attr('r', '5px')
-        .attr('cx', function(d) {
-          return xScale(d.perioden)
-        })
-        .attr('cy', function(d) {
-          return yScale(d.aantal)
-        })
-        .attr('fill', 'blue')
-        .attr('stroke-width', 1)
-        .attr('stroke', 'white')
+      // Update data amsterdam
+      updateLine(amsterdam, "path.amsterdam-line")
+      updateDots(amsterdam, "g.amsterdam-dots")
 
-      // Data for Utrecht
-      svg.append("path")
-        .datum(utrecht)
-        .attr('stroke', 'yellow')
-        .attr('stroke-width', 5)
-        .attr('fill', 'none')
-        .attr('d', line);
-      
-      svg.append('g').selectAll('circle')
-        .data(utrecht)
-        .enter()
-        .append('circle')
-        .attr('r', '5px')
-        .attr('cx', function(d) {
-          return xScale(d.perioden)
-        })
-        .attr('cy', function(d) {
-          return yScale(d.aantal)
-        })
-        .attr('fill', 'yellow')
-        .attr('stroke-width', 1)
-        .attr('stroke', 'white')
-      
-      d3.select('button').on('click', () => {
-        console.log('test')
-      })
-    } else {
-      console.log(err)
+      // Update data rotterdam
+      updateLine(rotterdam, "path.rotterdam-line")
+      updateDots(rotterdam, "g.rotterdam-dots")
+
+      // Update data denhaag
+      updateLine(denhaag, "path.denhaag-line")
+      updateDots(denhaag, "g.denhaag-dots")
+
+      // Update data utrecht
+      updateLine(utrecht, "path.utrecht-line")
+      updateDots(utrecht, "g.utrecht-dots")
     }
-  })
-
+  } else {
+    console.log(err)
+  }
+}
